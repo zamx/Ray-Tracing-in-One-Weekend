9985eb4df3dff8aa3fd0cde853f21e2973b56166
Sam Van Gucht <vangucht.sam@gmail.com>
2025-02-22 15:48:36 +0100

    Add math file

--- src/data.rs
+++ src/data.rs
@@ -2,6 +2,7 @@ pub mod image;
 pub mod color;
 pub mod vec3;
 pub mod ray;
+pub mod math;
 
 #[cfg(test)]
 mod tests;
---
+++ src/data/math.rs
@@ -0,0 +1,5 @@
+const PI: f64 = 3.1415926535897932385;
+
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * PI / 180.0
+}
